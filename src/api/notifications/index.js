9b54e98b575614b08a1cf9c58a305751fe90d4e2
itaym <itaymer@gmail.com>
2022-09-12 17:46:38 +0300

    api notifications end point

---
+++ src/api/notifications/index.js
@@ -0,0 +1,26 @@
+import delNotification from '@/src/api/notifications/delNotification'
+import getNotifications from '@/src/api/notifications/getNotifications'
+import httpStatus from 'http-status'
+import responseJson from '@/utils/serverOnly/responseJson'
+import { addUserToApi } from '@/src/utils/serverOnly'
+
+const notifications = async (req, res, user) => {
+    switch (req.method) {
+        case 'DELETE':
+            await delNotification(res, req.body._id)
+            break
+        case 'PATCH':
+            break
+        case 'GET':
+            await getNotifications(res, user)
+            break
+        case 'POST':
+            break
+        case 'UPDATE':
+            break
+        default:
+            res.status(httpStatus.METHOD_NOT_ALLOWED)
+               .json(responseJson(false, { }, httpStatus.METHOD_NOT_ALLOWED, 'errors.method_not_allowed'))
+    }
+}
+export default addUserToApi(notifications)
