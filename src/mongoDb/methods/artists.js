2d150bfa11df0d7f91b58f18d41a616fcaec21cb
Haim Kalvo <you@example.com>
2023-01-23 18:24:42 +0200

    fixing artist profile after code review

---
+++ src/mongoDb/methods/artists.js
@@ -0,0 +1,9 @@
+export const _getArtistProfile = async function (name) {
+
+    return {
+        name: "Itay Merchav",
+        singles: [{ name: "Tel-Aviv: Tmunat Matzav" }],
+        description: "While many bands sing of ideal romance unimaginable to the average Joe, Prentzip’s new single “Shir Pop” (Pop Song) does just the opposite. It embraces that average Joe in his everyday frustrations as he falls for a girl who just can’t seem to reciprocate. Instead of chasing after her time and time again with grand gestures and heartfelt love songs, all he can manage is an aromantic pop song. We asked Haim Kalvo, lead singer and songwriter of Prentzip, some questions about the band and the new single",
+        image: "/images/itay-img.png"
+    }
+}
